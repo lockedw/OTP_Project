27f64e106363bf7f557616241847808c86c0a09e
lockedw <104690046+lockedw@users.noreply.github.com>
2022-08-20 00:01:25 +0300

    Its not finish but i need to have sleep
    <3

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # OTP_Project
  a simple otp page with using html,css,js
+ and its full open source <3
---
+++ app.js
@@ -0,0 +1,20 @@
+const inputs = document.querySelector('.otp-field input')
+
+inputs.forEach((input, index) => {
+    input.dataset.index = index;
+    input.addEventListener('paste', handleOnPasteOtp);
+    input.addEventListener('keyup', handleOtp);
+
+})
+
+function handleOnPasteOtp(e) {
+    const data = e.clipboardData.getData('text')
+    const value = data.split('');
+    if(value.length == inputs.length){
+        inputs.forEach((input, index) => (input.value = value[index]));
+        submit();
+    }
+
+}
+
+function
